70006de428109a7f2d664708680906ec9eb5b62a
William Amaechi <amaechiwilliam18@gmail.com>
2023-01-16 15:52:59 -0500

    Added To Do Manager DAO class. This class will handle differnt cases in To Do Manager. Added comments onto Task Bean class and To Do Manager DAO class

--- src/com/cogent/toDoManager/Task.java
+++ src/com/cogent/toDoManager/Task.java
@@ -1,8 +1,14 @@
 package com.cogent.toDoManager;
-
+/**
+ * 
+ * @author: William U. Amaechi
+ * @date: 	Jan 16, 2023
+ * 
+ * Task beans holds the TaskID and the TaskName that our client will use in ToDoManager
+ */
 public class Task {
-	private int taskID;
-	private String taskName;
+	private int taskID; // The ID number for this Task Bean
+	private String taskName; // The name for this Task Bean
 	
 	public Task() {
 		this.taskID = 0;
@@ -12,5 +18,23 @@ public class Task {
 	public Task(int taskID, String taskName) {
 		this.taskID = taskID;
 		this.taskName = taskName;
+	}
+	
+	//getters for TaskID and TaskName
+	public int getTaskID() {
+		return taskID;
+	}
+	public String getTaskName(){
+		return taskName;
+	}
+	
+	//setters for TaskID and TaskName
+	public void setTaskID(int taskID) {
+		this.taskID = taskID;
+	}
+	
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+		
 	}
 }
---
+++ src/com/cogent/toDoManager/ToDoManagerDAO.java
@@ -0,0 +1,30 @@
+package com.cogent.toDoManager;
+/**
+ * 
+ * @author: William U. Amaechi
+ * @date: 	Jan 16, 2023
+ * ToDoManagerDAO tell ToDoManger specific instructions that will be done for each case in our Task Array
+ */
+public class ToDoManagerDAO {
+
+	// addTask: Adds a Task Bean to Task Array if Task Array is not full
+	public static void addTask() {
+
+	}
+
+	//updateTask: Update the TaskID or the TaskName of a Task Bean in our Task Array if the Bean is found in our Array
+	public static void updateTask() {
+
+	}
+
+	//deleteTask: Deletes a Task bean from our Task Array if Task Bean is found in Array
+	public static void deleteTask() {
+
+	}
+
+	//searchTask: Search for Task Bean in our Task Array and display the TaskID and TaskName of Task Bean if found in Array
+	public static void searchTask() {
+
+	}
+
+}
